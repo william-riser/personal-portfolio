085ece1805d1e2f30b3f07b38f1eb7ab8781a587
William Riser <riser.w@northeastern.edu>
2024-02-04 19:57:47 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center bg-blue-500 text-white'>
+       Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
